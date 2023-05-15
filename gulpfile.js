560be2b9fe612dbb8f3ecae552e2ed3566eb2f01
Marcus <crocushtml@gmail.com>
2023-05-15 13:31:53 +0200

    bug with gulp and styles was fixed

--- gulpfile.js
+++ gulpfile.js
@@ -7,64 +7,65 @@ const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
+
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "dist"
+            baseDir: "docs"
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+        return gulp.src("docs/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
-    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
-    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
-    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+    gulp.watch("docs/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("docs/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("docs/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("docs/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("docs/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("docs/img/**/*").on('all', gulp.parallel('images'));
 });
 
 gulp.task('html', function () {
-    return gulp.src("src/*.html")
+    return gulp.src("docs/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("dist/"));
+        .pipe(gulp.dest("docs/"));
 });
 
 gulp.task('scripts', function () {
-    return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"))
+    return gulp.src("docs/js/**/*.js")
+        .pipe(gulp.dest("docs/js"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function () {
-    return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"))
+    return gulp.src("docs/fonts/**/*")
+        .pipe(gulp.dest("docs/fonts"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('icons', function () {
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"))
+    return gulp.src("docs/icons/**/*")
+        .pipe(gulp.dest("docs/icons"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('images', function () {
-    return gulp.src("src/img/**/*")
+    return gulp.src("docs/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"))
+        .pipe(gulp.dest("docs/img"))
         .pipe(browserSync.stream());
 });
 
